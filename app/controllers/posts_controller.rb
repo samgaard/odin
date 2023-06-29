c5f83e2f598ab992714b268ee85d58b802fbdfd5
sam.gaard <sam.gaard@housecallpro.com>
2023-06-29 11:02:01 -0400

    work save

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,36 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = current_user.posts.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      flash[:warning] = 'Something Went Wrong :/'
+      redirect_to posts_path
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  resources :posts
+  get 'friendships/index'
+  get 'friendships/create'
+  post 'friendships/update', to: 'friendships#update'
+  get 'users/show'
+  get 'users/index'
+  devise_for :users
+  get 'home/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "home#index"
+end
