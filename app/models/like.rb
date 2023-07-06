51c9ffdec0002cf32533b694a0ba59784404483e
sam.gaard <sam.gaard@housecallpro.com>
2023-07-06 10:41:48 -0400

    seed friendships

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :friend, class_name: 'User'
-  belongs_to :post
+  belongs_to :post, dependent: :destroy
   # validate :friendship_required
   # def friendship_required
   #   errors.add(:base, 'Friendship required to like posts') unless post.author.is_friend(friend.id)
